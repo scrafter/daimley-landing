df3e52b6cbce93d6f8d1c2b6b737f7e11f52d1d0
tomaszmanka <tomasz.manka@codibly.com>
2022-01-23 21:43:26 +0100

    feat(cookies): cookies policy support

--- src/components/CookiesBar/CookiesBar.tsx
+++ src/components/CookiesBar/CookiesBar.tsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   CookiesBarStyled,
   Content,
   CloseButton,
 } from '@/components/CookiesBar/CookiesBar.styles';
 import useTranslation from '@/useTranslation';
+import Cookies from 'js-cookie';
+
+const COOKIE_NAME = `isCookiePolicyAccepted`;
 
 function CookiesBar() {
   const { translate } = useTranslation();
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const isAccepted = Cookies.get(COOKIE_NAME);
+    if (!isAccepted) {
+      setIsOpen(true);
+    }
+  }, []);
+
+  const onClose = () => {
+    Cookies.set(COOKIE_NAME, true, { expires: 30 });
+    setIsOpen(false);
+  };
 
   return isOpen ? (
     <CookiesBarStyled>
       <Content>{translate(`cookies.content`)}</Content>
 
-      <CloseButton onClick={() => setIsOpen(false)}>x</CloseButton>
+      <CloseButton onClick={onClose}>x</CloseButton>
     </CookiesBarStyled>
   ) : null;
 }
