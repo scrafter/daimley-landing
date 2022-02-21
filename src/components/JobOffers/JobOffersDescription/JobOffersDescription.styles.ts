d5501e8179a73b8de4d16998582f9d07b9960b07
tomaszmanka <tomasz.manka@codibly.com>
2022-02-21 22:57:42 +0100

    feat(job offers): description - mobile version

--- src/components/JobOffers/JobOffersDescription/JobOffersDescription.styles.ts
+++ src/components/JobOffers/JobOffersDescription/JobOffersDescription.styles.ts
@@ -5,11 +5,20 @@ export const JobOffersDescriptionStyled = styled.section`
   padding: 120px 20px 80px;
   display: flex;
   justify-content: center;
+
+  @media only screen and (max-width: 425px) {
+    flex-direction: column;
+  }
 `;
 
 export const DescriptionWrapper = styled.div`
   max-width: 650px;
   margin-left: 50px;
+
+  @media only screen and (max-width: 425px) {
+    margin-top: 50px;
+    margin-left: 0;
+  }
 `;
 
 export const Description = styled.p`
@@ -27,6 +36,7 @@ export const Title = styled.h2`
   font-size: 2.75rem;
   font-weight: bold;
   margin-bottom: 70px;
+  line-height: 4rem;
 
   & span {
     margin-left: 0.5rem;
