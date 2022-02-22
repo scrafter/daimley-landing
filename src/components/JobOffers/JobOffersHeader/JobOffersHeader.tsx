adbe0f89716b89c4633bdf00d1d4620621227a48
tomaszmanka <tomasz.manka@codibly.com>
2022-02-22 20:18:51 +0100

    fix(joboffers): header button text and behaviour

--- src/components/JobOffers/JobOffersHeader/JobOffersHeader.tsx
+++ src/components/JobOffers/JobOffersHeader/JobOffersHeader.tsx
@@ -6,6 +6,7 @@ import {
 } from '@/components/JobOffers/JobOffersHeader/JobOffersHeader.styles';
 import useTranslation from '@/useTranslation';
 import Button from '@/components/Shared/Button/Button';
+import { CV_VERIFY_EMAIL } from '@/components/MainPage/MainPageHeader/MainPageHeader';
 
 function JobOffersHeader() {
   const { translate } = useTranslation();
@@ -14,7 +15,10 @@ function JobOffersHeader() {
     <JobOffersHeaderStyled>
       <SubTitle>{translate(`jobOffers.header.subTitle`)}</SubTitle>
       <Title>{translate(`jobOffers.header.title`)}</Title>
-      <Button link="/form" label={translate(`recruitment.header.button`)} />
+      <Button
+        link={CV_VERIFY_EMAIL}
+        label={translate(`jobOffers.header.button`)}
+      />
     </JobOffersHeaderStyled>
   );
 }
--- src/components/JobOffers/JobOffersList/JobOffersList.tsx
+++ src/components/JobOffers/JobOffersList/JobOffersList.tsx
@@ -10,12 +10,12 @@ import { JOB_OFFERS } from '@/components/JobOffers/JobOffersList/jobOffers';
 import TrainingItem from '@/components/Trainings/AboutTrainings/TrainingItem/TrainingItem';
 
 const LABELS = [
-  `jobOffers.header.button`,
-  `jobOffers.header.label1`,
-  `jobOffers.header.label2`,
-  `jobOffers.header.label3`,
-  `jobOffers.header.label4`,
-  `jobOffers.header.label5`,
+  `jobOffers.list.button`,
+  `jobOffers.list.label1`,
+  `jobOffers.list.label2`,
+  `jobOffers.list.label3`,
+  `jobOffers.list.label4`,
+  `jobOffers.list.label5`,
 ];
 
 function JobOffersList() {
